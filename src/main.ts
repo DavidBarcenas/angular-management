3e03350af2baff463fb59b0e3f0b452244fdc53e
David Barcenas <daveepro@outlook.com>
2021-05-04 20:21:26 -0500

    integrate Swagger and PartialType with OpenAPI to document services

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -10,6 +11,16 @@ async function bootstrap() {
       forbidNonWhitelisted: true, // mostrará un problema cuando se envien atributos demas
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('API - Nestjs Course')
+    .setDescription('The Nestjs Course API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
