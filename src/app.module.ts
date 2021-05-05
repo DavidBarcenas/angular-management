5f2e170a427e8d7e5d010151df2e694f479a89bb
David Barcenas <daveepro@outlook.com>
2021-05-04 19:49:22 -0500

    create typing for environments variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,14 @@ import { DatabaseModule } from './database/database.module';
 import { environments } from './environments';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
+import config from './config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: environments[process.env.NODE_ENV] || '.env',
       isGlobal: true,
+      load: [config],
     }),
     HttpModule,
     ProductsModule,
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import config from './config';
 
 @Injectable()
 export class AppService {
-  constructor(private configService: ConfigService) {}
+  constructor(
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
+  ) {}
 
   getHello(): any {
     return {
-      key: this.configService.get('API_KEY'),
+      key: this.configService.apiKey,
     };
   }
 }
