ea469ea7f0f58e953fc20999aad302fb9102d178
David Barcenas <daveepro@outlook.com>
2021-05-04 19:12:47 -0500

    use global module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,13 @@
-import { HttpModule, HttpService, Module } from '@nestjs/common';
+import { HttpModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 
-const API_KEY = '123456878';
-const API_KEY_PROD = 'PROD123456878';
-
 @Module({
-  imports: [HttpModule, ProductsModule, UsersModule],
+  imports: [HttpModule, ProductsModule, UsersModule, DatabaseModule],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: 'API_KEY',
-      useValue: process.env.NODE_env === 'prod' ? API_KEY_PROD : API_KEY,
-    },
-    {
-      provide: 'TASKS',
-      useFactory: async (http: HttpService) => {
-        const tasks = await http
-          .get('https://jsonplaceholder.typicode.com/todos')
-          .toPromise();
-        return tasks.data;
-      },
-      inject: [HttpService],
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
