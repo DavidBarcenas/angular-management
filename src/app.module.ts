93cf230a0ee8d4ef1d1385b871d4e11f92c29ec1
David Barcenas <daveepro@outlook.com>
2021-05-04 20:09:22 -0500

    validate schemas in .envs with Joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { HttpModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
@@ -14,6 +16,11 @@ import config from './config';
       envFilePath: environments[process.env.NODE_ENV] || '.env',
       isGlobal: true,
       load: [config],
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
     }),
     HttpModule,
     ProductsModule,
