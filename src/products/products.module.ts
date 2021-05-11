7671d5ec14f3207a95bc3a18e31e4f0def5bfe0d
David Barcenas <daveepro@outlook.com>
2021-05-10 20:41:17 -0500

    create product identity

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { BrandsController } from './controllers/brands.controller';
-import { CategoriesController } from './controllers/categories.controller';
-import { ProductsController } from './controllers/products.controller';
 import { BrandService } from './services/brands.service';
+import { CategoriesController } from './controllers/categories.controller';
 import { CategoriesService } from './services/categories.service';
+import { ProductsController } from './controllers/products.controller';
 import { ProductsService } from './services/products.service';
+import { Product } from './entities/product.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product])],
   controllers: [ProductsController, CategoriesController, BrandsController],
   providers: [ProductsService, BrandService, CategoriesService],
   exports: [ProductsService],
