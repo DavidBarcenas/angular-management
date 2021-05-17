d991f43a2fe955e541ed678470fa8c27a32de5b8
David Barcenas <daveepro@outlook.com>
2021-05-17 10:41:04 -0500

    add public paths and custom decorator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,21 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, SetMetadata, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './auth/decorators/public.decorator';
 import { ApiKeyGuard } from './auth/guards/api-key.guard';
 
+@UseGuards(ApiKeyGuard)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @UseGuards(ApiKeyGuard)
   @Get()
   getTasks(): string {
+    return this.appService.getTasks();
+  }
+
+  @Get('notGuard')
+  @Public()
+  notGuard(): string {
     return this.appService.getTasks();
   }
 }
