d28fd962cd7392365210afd52f645df9e399ba76
David Barcenas <dbarcenas.dev@gmail.com>
2023-06-05 16:30:58 -0600

    tasks: add service methods

--- tasks/src/tasks.service.ts
+++ tasks/src/tasks.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Task } from './schemas/task.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(@InjectModel(Task.name) private readonly taskModel: Model<Task>) {}
+
+  findAll() {
+    return this.taskModel.find();
+  }
+
+  async findById(id: string) {
+    return this.taskModel.findById(id);
+  }
+
+  async create(task: Task) {
+    const newTask = new this.taskModel(task);
+    return newTask.save();
+  }
+
+  async update(id: string, task: Task) {
+    return this.taskModel.findByIdAndUpdate(id, task);
+  }
+
+  async delete(id: string) {
+    return this.taskModel.findByIdAndDelete(id);
+  }
+}
