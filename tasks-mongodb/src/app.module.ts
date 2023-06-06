50d425118dc9664f17079d9d45f77130561208f6
David Barcenas <dbarcenas.dev@gmail.com>
2023-06-05 20:28:55 -0600

    tasks: add schema validation

--- tasks-mongodb/src/app.module.ts
+++ tasks-mongodb/src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { TasksModule } from './tasks/tasks.module';
+import { DatabaseModule } from './database/database.module';
+import {ConfigModule} from "@nestjs/config";
+import {environments} from "./environments";
+import databaseConfig from "./database/database.config";
+import {dbValidationSchema} from "./schemas/validation.schema";
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), TasksModule]
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: environments[process.env.NODE_ENV] || '.env',
+      isGlobal: true,
+      load: [databaseConfig],
+      validationSchema: dbValidationSchema
+    }),
+    DatabaseModule
+  ]
 })
 export class AppModule {}
